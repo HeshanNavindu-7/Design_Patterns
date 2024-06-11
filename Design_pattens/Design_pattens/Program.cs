450d10f1729c372f36ff89339a12125877c9d5c4
Heshan Navindu <121144605+HeshanNavindu-7@users.noreply.github.com>
2024-06-11 23:46:57 +0530

    first

---
+++ Design_pattens/Design_pattens/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Design_pattens
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+             Console.WriteLine("Singleton Pattern Demonstration:");
+
+            // Accessing the singleton instance
+            Singleton instance1 = Singleton.Instance;
+            Singleton instance2 = Singleton.Instance;
+
+            // Check if both references point to the same instance
+            if (instance1 == instance2)
+            {
+                Console.WriteLine("Both references point to the same instance.");
+            }
+            else
+            {
+                Console.WriteLine("Different instances exist.");
+            }
+
+            // Call a method on the singleton instance
+            instance1.DoSomething();
+        }
+    }
+}
---
+++ Design_pattens/Design_pattens/Sigleton_pattern/Singleton.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Design_pattens
+{
+    public class Singleton
+    {
+        private static Singleton _instance;
+        private Singleton() {
+        
+        }
+        public static Singleton Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new Singleton();
+                }
+                return _instance;
+            }
+        }
+       public void DoSomething()
+        {
+            Console.WriteLine("Singleton instance is Work");
+        }
+
+    }
+}
